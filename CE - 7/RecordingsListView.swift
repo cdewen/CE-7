5659bd4876fb437790ffac59845d413ada8b2b03
Carter Ewen <carter.ewen03@gmail.com>
2026-02-24 12:00:47 -0500

    added icon and fixed time formatting

--- CE - 7/RecordingsListView.swift
+++ CE - 7/RecordingsListView.swift
@@ -72,10 +72,7 @@ struct RecordingsListView: View {
         let hours = totalSeconds / 3600
         let minutes = (totalSeconds % 3600) / 60
         let seconds = totalSeconds % 60
-        if hours > 0 {
-            return String(format: "%d:%02d:%02d", hours, minutes, seconds)
-        }
-        return String(format: "%d:%02d", minutes, seconds)
+        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
     }
 }
 
